d0b42dc5ccf89423f7043abf91c096609d063efc
Aryan Srivastava <aryansrivas123@gmail.com>
2022-11-13 15:30:24 +0530

    Some changes and captcha insert added

--- cap_gen.py
+++ cap_gen.py
@@ -1,9 +1,16 @@
 from tkinter import*
-top=Tk()
-top.geometry('450x220')
-top.title("Login")
-Reg=Label(top,text='Registration Number: ',font='Times 13')
-Reg.grid(row=1,column=10,sticky=E)
-Reg_ent=Entry(top)
-Reg_ent.grid(row=1,column=11)
-top.mainloop()
+# defining the geometry of parent window
+root=Tk()
+root.geometry('450x220')
+root.title("Login")
+# designing the registration box
+Reg_no=Label(root,text='Registration Number: ',font='calibri 15 bold')
+Reg_no.grid(row=1,column=10,sticky=E)
+Regno_ent=Entry(root)
+Regno_ent.grid(row=1,column=11)
+# adding field for captcha writing.
+ent=Label(root,text='Enter the Captcha: ',font='calibri 15 bold')
+ent.grid(row=6,column=10)
+ent_cap=Entry(root)
+ent_cap.grid(row=6,column=11)
+root.mainloop()
