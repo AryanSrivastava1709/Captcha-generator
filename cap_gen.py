1378d7d905c998b6930234b4788a5344baf46eed
Aryan Srivastava <aryansrivas123@gmail.com>
2022-11-13 15:10:41 +0530

    Registration box added

--- cap_gen.py
+++ cap_gen.py
@@ -2,4 +2,8 @@ from tkinter import*
 top=Tk()
 top.geometry('450x220')
 top.title("Login")
+Reg=Label(top,text='Registration Number: ',font='Times 13')
+Reg.grid(row=1,column=10,sticky=E)
+Reg_ent=Entry(top)
+Reg_ent.grid(row=1,column=11)
 top.mainloop()
